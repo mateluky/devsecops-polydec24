2fd41b287302ded59abeaf67a08b32e0da2b515f
Máté Lukács <lukacsmate09@gmail.com>
2025-12-15 17:07:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const PASSWORD = "louvres2025"
+const PASSWORD = process.env.PASSWORD
 
 const inter = Inter({ subsets: ['latin'] })
 
